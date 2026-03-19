6fe6a2cb358c634b429972191e4335b8cf4f7b83
John <johneliud4@gmail.com>
2026-03-19 14:21:40 +0300

    feat: create OrderItem model for order line items. This update adds an OrderItem value object containing product details, pricing, and quantity for each item in an order.

---
+++ src/main/java/io/github/johneliud/order_service/models/OrderItem.java
@@ -0,0 +1,17 @@
+package io.github.johneliud.order_service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    private String productId;
+    private String productName;
+    private BigDecimal price;
+    private Integer quantity;
+}
