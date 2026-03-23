7f3f130e4a98e679c6f438ab69278bdf36b63c8f
John <johneliud4@gmail.com>
2026-03-23 11:26:50 +0300

    feat(order): add order items to OrderStatusChangedEvent for stock management on delivery.

--- src/main/java/io/github/johneliud/order_service/dto/OrderStatusChangedEvent.java
+++ src/main/java/io/github/johneliud/order_service/dto/OrderStatusChangedEvent.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,5 @@ public class OrderStatusChangedEvent {
     private String sellerId;
     private String oldStatus;
     private String newStatus;
+    private List<OrderItemEvent> items;
 }
