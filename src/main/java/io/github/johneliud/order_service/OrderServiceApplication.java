de32079ea1fda97023b72f7f4b73428ca3088c7f
John <johneliud4@gmail.com>
2026-03-23 13:50:57 +0300

    feat(order-service): enable Spring Retry for resilient HTTP client calls to product service.

--- src/main/java/io/github/johneliud/order_service/OrderServiceApplication.java
+++ src/main/java/io/github/johneliud/order_service/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package io.github.johneliud.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
